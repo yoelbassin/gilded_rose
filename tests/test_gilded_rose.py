2915954d2b99d70fe3c5f3c6908ceafdeb1d3bee
yoelbassin <bassin.yoel@gmail.com>
2024-03-09 18:05:38 +0200

    Create basic conjured test

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -7,7 +7,10 @@ def create_item_and_update_quality(
     name: str = "foo",
     sell_in: int = 10,
     quality: int = 10,
+    conjured: bool = False
 ) -> Item:
+    if conjured:
+        name = f"Conjured {name}"
     item = Item(name, sell_in, quality)
     gilded_rose = GildedRose((item,))
     gilded_rose.update_quality()
@@ -70,15 +73,30 @@ def test_backstage_passes_quality_degrading(
 
 
 @pytest.mark.parametrize(
-    "initial_quality, initial_sell_in, expected_quality",
-    [(10, 5, 10), (10, 0, 10)]
+    "initial_quality, initial_sell_in, expected_quality", [(10, 5, 10), (10, 0, 10)]
 )
-def test_sulfuras(
-    initial_quality: int, initial_sell_in: int, expected_quality: int
-):
+def test_sulfuras(initial_quality: int, initial_sell_in: int, expected_quality: int):
     item = create_item_and_update_quality(
         name="Sulfuras, Hand of Ragnaros",
         sell_in=initial_sell_in,
         quality=initial_quality,
     )
     assert item.quality == expected_quality
+
+
+@pytest.mark.parametrize(
+    "initial_quality, initial_sell_in, expected_quality",
+    [
+        (10, 10, 8), (0, 10, 0), (10, 0, 6), (10, -2, 6),
+    ],
+)
+def test_basic_conjured_item(
+    initial_quality: int, initial_sell_in: int, expected_quality: int
+):
+    item = create_item_and_update_quality(
+        conjured=True,
+        sell_in=initial_sell_in,
+        quality=initial_quality,
+    )
+    assert item.quality == expected_quality 
+    
