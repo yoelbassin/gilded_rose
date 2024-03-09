94ab0abde8d345f4a1bd7badc72fe949615e21cc
yoelbassin <bassin.yoel@gmail.com>
2024-03-09 17:20:16 +0200

    Create basic quality and sell_in degrading tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
-
+import pytest
 from gilded_rose.gilded_rose import Item, GildedRose
 
 
-
-def test_regular_item():
-    item = Item("foo", 10, 10)
+@pytest.mark.parametrize(
+    "initial_quality, initial_sell_in, expected_quality",
+    [(10, 10, 9), (0, 10, 0), (10, 0, 8), (10, -2, 8)],
+)
+def test_basic_quality_degrading(
+    initial_quality: int, initial_sell_in: int, expected_quality: int
+):
+    item = Item("foo", initial_sell_in, initial_quality)
     gilded_rose = GildedRose((item,))
     gilded_rose.update_quality()
-    
-    assert item.quality == 9
-    assert item.sell_in == 9
+
+    assert item.quality == expected_quality
+
+
+@pytest.mark.parametrize(
+    "initial_sell_in, expected_sell_in", [(10, 9), (0, -1), (-5, -6)]
+)
+def test_basic_sell_in_degrading(initial_sell_in: int, expected_sell_in: int):
+    item = Item("foo", initial_sell_in, 10)
+    gilded_rose = GildedRose((item,))
+    gilded_rose.update_quality()
+
+    assert item.sell_in == expected_sell_in
